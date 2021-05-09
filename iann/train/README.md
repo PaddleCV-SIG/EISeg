3921e5fd4fd55c8f2329d45f50d33f1bc00d0a2d
geoyee <geoyee@yeah.net>
2021-05-09 17:15:29 +0800

    update Train

--- iann/train/README.md
+++ iann/train/README.md
@@ -1,13 +1,19 @@
 # 训练iann可用的自定义模型
 
-目前初版只能完成简单的训练，各种设置和数据集格式都比较死。
+目前初步完成训练和简单的配置，但是其中有点设置和数据集格式都还比较死，还有很大的改进空间。
 
 ## 一、数据组织
 
 在需要训练自己的数据集时，目前需要将数据集构造为如下格式，直接放在datasets文件夹中。
 
 ```
-DatasetName
+trainDataset
+     ├── img
+     |    └── filename.jpg
+     └── gt
+          └── filename.png
+          
+evalDataset
      ├── img
      |    └── filename.jpg
      └── gt
@@ -16,7 +22,44 @@ DatasetName
 
 ## 二、训练
 
-直接运行ritm_train.py即可开始训练。目前其他超参数和模型选择都在ritm_train.py中，下一步要把这些提出来作为配置文件。
+直接运行ritm_train.py即可开始训练。目前一些简单的参数已经可以在yaml配置文件中进行自定义设置，不过现阶段不够灵活，也可能容易出现问题。
+
+```
+iters: 100000  # 训练轮数
+batch_size: 16  # bs大小
+save_interval: 1000  # 保存间隔
+log_iters: 10  # 打印log的间隔
+worker: 4  # 子进程数
+save_dir: model_output  # 保存路径
+use_vdl: False  # 是否使用vdl
+
+train_dataset:  # 训练数据
+  dataset_path: iann/train/datasets/egoHands  # 数据路径
+  crop_size: [320, 480]  # 裁剪尺寸
+
+val_dataset:  # 验证数据
+  dataset_path: iann/train/datasets/testData  # 数据路径
+
+optimizer:
+  type: adam  # 优化器，目前仅可以选择‘adam’和‘sgd’
+
+learning_rate:
+  value_1: 5e-5  # 需要设置两个学习率
+  value_2: 5e-6
+  decay:
+    type: poly  # 学习率衰减，目前仅支持‘poly’，可以修改下面的参数
+    steps: 1000
+    power: 0.9
+    end_lr: 0.0
+
+model:
+  type: deeplab  # 模型名称，目前支持‘hrnet’、‘deeplab’以及‘shufflenet’
+  backbone: resnet18  # 下面的参数是模型对应的参数，可在源码中查看
+  is_ritm: True
+  weights: None  # 加载权重的路径
+```
+
+
 
 ### * 说明
 
--- iann/train/ritm_train.py
+++ iann/train/ritm_train.py
@@ -1,86 +1,71 @@
 import os
-import argparse
-import random
-from easydict import EasyDict as edict
 import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import random
+import argparse
+from easydict import EasyDict as edict
 
 import paddle
 import paddle.nn as nn
 import paddleseg.transforms as T
-from paddleseg.utils import logger
-from model.model import get_hrnet_model, DistMapsHRNetModel, get_deeplab_model, get_shufflenet_model
-from model.modeling.hrnet_ocr import HighResolutionNet
+from paddleseg.utils import logger, get_sys_env, logger
 from albumentations import (
     Compose, ShiftScaleRotate, PadIfNeeded, RandomCrop,
     RGBShift, RandomBrightnessContrast, RandomRotate90, HorizontalFlip
 )
-from data.points_sampler import MultiPointSampler
-#from data.points_sampler_ritm import MultiPointSampler
-from data.davis import DavisDataset
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from model.model import get_hrnet_model, DistMapsHRNetModel, get_deeplab_model, get_shufflenet_model
+from model.modeling.hrnet_ocr import HighResolutionNet
 from model.loss import *
+from data.points_sampler import MultiPointSampler
+from data.davis import DavisDataset
+from util.config import cfgData
 from util.util import *
-from paddleseg.utils import get_sys_env, logger
-from visualdl import LogWriter
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--max_iters', type=int, default=1001,
-                        help='The number of the starting epoch from which training will continue. '
-                             '(it is important for correct logging and learning rate)')
-
-    parser.add_argument('--workers', type=int, default=4,
-                        metavar='N', help='Dataloader threads.')
-
-    parser.add_argument('--batch_size', type=int, default=4,
-                        help='You can override model batch size by specify positive number.')
-
-    parser.add_argument('--weights', type=str, default=None,
-                        help='Model weights will be loaded from the specified path if you use this argument.')
-
-    parser.add_argument('--use_vdl', type=bool, default=False,
-                        help='Whether to use visual dl.')
-
+    parser.add_argument(
+        "--config", dest="cfg", type=str, \
+        default='iann/train/train_config.yaml', help='The config file.')
     return parser.parse_args()
 
 
 def main():
     env_info = get_sys_env()
     info = ['{}: {}'.format(k, v) for k, v in env_info.items()]
-    info = '\n'.join(['', format('Environment Information', '-^48s')] + info +
-                     ['-' * 48])
+    info = '\n'.join(['', format('Environment Information', '-^48s')] + info + ['-' * 48])
     logger.info(info)
-
-    place = 'gpu' if env_info['Paddle compiled with cuda'] and env_info[
-        'GPUs used'] else 'cpu'
-
+    place = 'gpu' if env_info['Paddle compiled with cuda'] and \
+            env_info['GPUs used'] else 'cpu'
     paddle.set_device(place)
     nranks = paddle.distributed.ParallelEnv().nranks
     local_rank = paddle.distributed.ParallelEnv().local_rank
-    cfg = parse_args()
+
+    cfg = cfgData(parse_args().cfg)
     model_cfg = edict()
-    model_cfg.crop_size = (320, 480)
     model_cfg.input_normalization = {
         'mean': [.485, .456, .406],
         'std': [.229, .224, .225]
     }
     model_cfg.num_max_points = 10
-    model_cfg.input_transform = T.Compose([T.Normalize(mean=model_cfg.input_normalization['mean'],
-                                                       std=model_cfg.input_normalization['std'])], to_rgb=False)
-
+    model_cfg.input_transform = T.Compose([
+        T.Normalize(mean=model_cfg.input_normalization['mean'],
+                    std=model_cfg.input_normalization['std'])],
+                    to_rgb=False)
     nn.initializer.set_global_initializer(nn.initializer.Normal(), nn.initializer.Constant())
-
-    #model = get_hrnet_model(width=18, ocr_width=64, with_aux_output=True, is_ritm=True)
-    #model = get_shufflenet_model()
-    #model.load_weights('/mnt/haoyuying/ritm_paddle_mac/pretrained/shufflenet_humanseg.pdparams')
-    model = get_deeplab_model(backbone='resnet18', is_ritm=True)
-    #model.load_weights('human_best/resnet18_ritm_95.5/model.pdparams')
-    #model.load_weights('/mnt/haoyuying/fbrs_interactive_segmentation/pretrained_models/hrnetv2_w18_imagenet_model.pdparams')
+    models = cfg.get('model')
+    if models.get('type') == 'deeplab':
+        model = get_deeplab_model(backbone=models.get('backbone'), is_ritm=models.get('is_ritm'))
+    elif models.get('type') == 'hrnet':
+        model = get_hrnet_model(width=models.get('width'), ocr_width=models.get('ocr_width'), \
+                                with_aux_output=models.get('with_aux_output'), is_ritm=models.get('is_ritm'))
+    elif models.get('type') == 'shufflenet':
+        model = get_shufflenet_model()
+    if models.get('weights') != 'None':
+        model.load_weights(models.get('weights'))
     backbone_params, other_params = model.get_trainable_params()
-    #model.load_weights('/mnt/haoyuying/fbrs_paddle/output_1/iter_11000_89.04/model.pdparams')
 
     if nranks > 1:
         if not paddle.distributed.parallel.parallel_helper._is_parallel_ctx_initialized():
@@ -88,22 +73,24 @@ def main():
             ddp_net = paddle.DataParallel(model)
         else:
             ddp_net = paddle.DataParallel(model)
-        train(ddp_net, cfg, model_cfg, max_iters=cfg.max_iters, backbone_params=backbone_params,
+        train(ddp_net, cfg, model_cfg, backbone_params=backbone_params,
               other_params=other_params)
     else:
-        train(model, cfg, model_cfg, max_iters=cfg.max_iters, backbone_params=backbone_params,
+        train(model, cfg, model_cfg, backbone_params=backbone_params,
               other_params=other_params)
 
-def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', backbone_params=None,
-          other_params=None):
-    local_rank = paddle.distributed.ParallelEnv().local_rank
-    cfg.batch_size = 16 if cfg.batch_size < 1 else cfg.batch_size
-    cfg.val_batch_size = cfg.batch_size
-    cfg.input_normalization = model_cfg.input_normalization
-    crop_size = model_cfg.crop_size
 
-    log_iters = 10
-    save_interval = 1000
+def train(model, cfg, model_cfg, backbone_params=None, other_params=None):
+    local_rank = paddle.distributed.ParallelEnv().local_rank
+
+    max_iters = cfg.get('iters')
+    save_dir = cfg.get('save_dir')
+    batch_size = cfg.get('batch_size') if cfg.get('batch_size') > 1 else 1
+    val_batch_size = batch_size
+    input_normalization = model_cfg.input_normalization
+    crop_size = cfg.get('train_dataset').get('crop_size')
+    log_iters = cfg.get('log_iters')
+    save_interval = cfg.get('save_interval')
     num_masks = 1
 
     train_augmentator = Compose([
@@ -114,7 +101,6 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
         RandomBrightnessContrast(brightness_limit=(-0.25, 0.25), contrast_limit=(-0.15, 0.4), p=0.75),
         RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10, p=0.75)
     ], p=1.0)
-
     val_augmentator = Compose([
         PadIfNeeded(min_height=crop_size[0], min_width=crop_size[1], border_mode=0),
         RandomCrop(*crop_size)
@@ -123,11 +109,11 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
     def scale_func(image_shape):
         return random.uniform(0.75, 1.25)
 
-    points_sampler = MultiPointSampler(model_cfg.num_max_points, prob_gamma=0.7,
-                                       merge_objects_prob=0.15,
-                                       max_num_merged_objects=2)
+    points_sampler = MultiPointSampler(
+        model_cfg.num_max_points, prob_gamma=0.7,
+        merge_objects_prob=0.15, max_num_merged_objects=2)
     trainset = DavisDataset(
-        'iann/train/datasets/egoHands',
+        cfg.get('train_dataset').get('dataset_path'),
         num_masks=num_masks,
         augmentator=train_augmentator,
         points_from_one_object=False,
@@ -139,9 +125,8 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
         samples_scores_path=None,
         samples_scores_gamma=1.25
     )
-
     valset = DavisDataset(
-        'iann/train/datasets/egoHands',
+        cfg.get('val_dataset').get('dataset_path'),
         augmentator=val_augmentator,
         num_masks=num_masks,
         points_from_one_object=False,
@@ -150,39 +135,55 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
         image_rescale=scale_func,
         points_sampler=points_sampler
     )
-
     batch_sampler = paddle.io.DistributedBatchSampler(
-        trainset, batch_size=cfg.batch_size, shuffle=True, drop_last=True)
-
+        trainset, batch_size=batch_size, shuffle=True, drop_last=True)
     loader = paddle.io.DataLoader(
         trainset,
         batch_sampler=batch_sampler,
         return_list=True,
     )
-
     val_batch_sampler = paddle.io.DistributedBatchSampler(
-        valset, batch_size=cfg.batch_size, shuffle=True, drop_last=True)
-
+        valset, batch_size=batch_size, shuffle=True, drop_last=True)
     val_loader = paddle.io.DataLoader(
         valset,
         batch_sampler=val_batch_sampler,
         return_list=True,
     )
 
-    if cfg.use_vdl:
+    if cfg.get('use_vdl'):
         from visualdl import LogWriter
         log_writer = LogWriter(save_dir)
 
     iters_per_epoch = len(batch_sampler)
 
-    optimizer1 = paddle.optimizer.Adam(learning_rate=5e-5, parameters=other_params)
-    optimizer2 = paddle.optimizer.Adam(learning_rate=5e-6, parameters=backbone_params)
+    opt = None
+    if cfg.get('optimizer').get('type') == 'adam':
+        opt = paddle.optimizer.Adam
+    elif cfg.get('optimizer').get('type') == 'sgd':
+        opt = paddle.optimizer.SGD
+    else:
+        raise ValueError('Opt only have adam or sgd now.')
+    lr = None
+    if cfg.get('learning_rate').get('decay').get('type') == 'poly':
+        lr = paddle.optimizer.lr.PolynomialDecay
+    else:
+        raise ValueError('Lr only have poly now.')
+    optimizer1 = opt(learning_rate=lr(float(cfg.get('learning_rate').get('value_1')),
+        decay_steps=cfg.get('learning_rate').get('decay').get('steps'), 
+        end_lr=cfg.get('learning_rate').get('decay').get('end_lr'), 
+        power=cfg.get('learning_rate').get('decay').get('power')),
+        parameters=other_params)
+    optimizer2 = opt(learning_rate=lr(float(cfg.get('learning_rate').get('value_1')),
+        decay_steps=cfg.get('learning_rate').get('decay').get('steps'), 
+        end_lr=cfg.get('learning_rate').get('decay').get('end_lr'), 
+        power=cfg.get('learning_rate').get('decay').get('power')),
+        parameters=backbone_params)
     instance_loss = NormalizedFocalLossSigmoid(alpha=0.5, gamma=2)
     instance_aux_loss = SigmoidBinaryCrossEntropyLoss()
     model.train()
-#     with open('mobilenet_model.txt', 'w') as f:
-#         for keys, values in model.state_dict().items():
-#             f.write(keys +'\t'+str(values.shape)+"\n")
+    # with open('mobilenet_model.txt', 'w') as f:
+    #     for keys, values in model.state_dict().items():
+    #         f.write(keys +'\t'+str(values.shape)+"\n")
     iters = 0
     avg_loss = 0.0
     while iters < max_iters:
@@ -200,9 +201,9 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
                 masks = masks.reshape([batch_size * num_points, c, h, w])
 
             output = batch_forward(model, images, masks, points)
-            #output = model(images, points)
-#             print('instance', output['instances'])
-#             print('mask', masks)
+            # output = model(images, points)
+            # print('instance', output['instances'])
+            # print('mask', masks)
             
             loss = instance_loss(output['instances'], masks)
             if 'instances_aux' in output.keys():
@@ -225,7 +226,7 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
                 avg_loss /= log_iters
                 logger.info('Epoch={}, Step={}/{}, loss={:.4f}, lr={}'.format(
                     (iters - 1) // iters_per_epoch + 1, iters, max_iters, avg_loss, lr))
-                if cfg.use_vdl:
+                if cfg.get('use_vdl'):
                     log_writer.add_scalar('Train/loss', avg_loss, iters)
                     log_writer.add_scalar('Train/lr', lr, iters)
                 avg_loss = 0.0
@@ -242,23 +243,22 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
                     if val_masks is not None:
                         val_batch_size, val_num_points, val_c, val_h, val_w = val_masks.shape
                         val_masks = val_masks.reshape([val_batch_size * val_num_points, val_c, val_h, val_w])
-                        
-                    val_output = batch_forward(model, val_images, val_masks, val_points, is_train=False)['instances']
-                        
-                    #val_output = model(val_images, val_points)['instances']
-#                     print('max', paddle.max(val_output))
-#                     print('output shape', val_output.shape)
+                    val_output = batch_forward(model, val_images, val_masks, val_points, is_train=False)['instances']    
+                    # val_output = model(val_images, val_points)['instances']
+                    # print('max', paddle.max(val_output))
+                    # print('output shape', val_output.shape)
                     val_output = nn.functional.interpolate(val_output, mode='bilinear', align_corners=True,
                                                size=val_masks.shape[2:])
                     val_output = val_output > 0.5
                     iter_iou = get_iou(val_masks.numpy(), val_output.numpy())
                     val_iou += iter_iou
                 logger.info('mean iou of iter {} is {}'.format(iters, val_iou / total_len))
-                if cfg.use_vdl:
+
+                if cfg.get('use_vdl'):
                     log_writer.add_scalar('Eval/miou', val_iou / total_len, iters)
 
-                current_save_dir = os.path.join(save_dir,
-                                                "iter_{}".format(iters))
+                current_save_dir = os.path.join(save_dir, "iter_{}".format(iters))
+
                 if not os.path.isdir(current_save_dir):
                     os.makedirs(current_save_dir)
                 paddle.save(model.state_dict(),
@@ -267,31 +267,23 @@ def train(model, cfg, model_cfg, max_iters, save_epoch=10, save_dir='hand', back
 
 
 def batch_forward(model, image, gt_mask, points, is_train=True):
-
     orig_image, orig_gt_mask = image.clone(), gt_mask.clone()
     prev_output = paddle.zeros_like(image, dtype='float32')[:, :1, : :]
-    
-    #last_click_indx = None
+    # last_click_indx = None
     num_iters = random.randint(1, 3)
     if is_train:
         model.eval()
     with paddle.no_grad():
         for click_indx in range(num_iters):
-
             net_input = paddle.concat([image, prev_output], axis=1)
-#             print(4)
             prev_output = model(net_input, points)['instances']
-#             print(5)
             prev_output = nn.functional.sigmoid(prev_output)
-#             print(6)
             points = get_next_points(prev_output, orig_gt_mask, points, click_indx + 1)
     if is_train:
         model.train()
     net_input = paddle.concat([image, prev_output], axis=1)
-
     output = model(net_input, points)
     return output
-
 
 
 if __name__ == '__main__':
