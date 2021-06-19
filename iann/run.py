99868339241c703ee116686f0cc46af01d127617
GT-Zhang <1029550448@qq.com>
2021-06-20 05:35:32 +0800

    增加对QPT打包程序的适配

---
+++ iann/run.py
@@ -0,0 +1,11 @@
+import sys
+
+from qtpy.QtWidgets import QApplication  # 导入PyQt相关模块
+from app import APP_IANN  # 导入带槽的界面
+
+
+def main():
+    app = QApplication(sys.argv)
+    myWin = APP_IANN()  # 创建对象
+    myWin.showMaximized()  # 全屏显示窗口
+    sys.exit(app.exec_())
