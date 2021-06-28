1b856eebaeb5b86b76f8fbf36e12d75d5fe52469
Lin Han <lh3317@nyu.edu>
2021-06-29 04:04:50 +0800

    fix satrting from cmd

--- eiseg/util/colormap.py
+++ eiseg/util/colormap.py
@@ -1,12 +1,15 @@
 import os.path as osp
 import random
+from eiseg import pjpath
 
 
 class ColorMask(object):
-    def __init__(self, color_path="config/colormap.txt", shuffle=False):
+    def __init__(
+        self, color_path=osp.join(pjpath, "config/colormap.txt"), shuffle=False
+    ):
         self.color_maps = []
         self.index = 0
-        with open(color_path, 'r') as f:
+        with open(color_path, "r") as f:
             self.color_maps = f.readlines()
         if shuffle:
             random.shuffle(self.color_maps)
@@ -20,5 +23,5 @@ class ColorMask(object):
         return self.to_list(color)
 
     def to_list(self, color):
-        r, g, b = color.split(',')
+        r, g, b = color.split(",")
         return [int(r), int(g), int(b)]
