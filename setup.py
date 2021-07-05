a6d8bf3b2b598ac3d0a17097ec51a8b5badbceea
geoyee <geoyee@yeah.net>
2021-07-05 16:50:36 +0800

    update load recent file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="EISeg",
-    version="0.1.6",
+    version="0.1.5",
     description="交互式标注软件",
     long_description=README,
     long_description_content_type="text/markdown",
