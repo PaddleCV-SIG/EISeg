87d1087bfdaecef929d96c378f334704b1b15214
Lin Han <lh3317@nyu.edu>
2021-07-11 18:20:57 +0800

    fix cv2 version

--- eiseg/util/polygon.py
+++ eiseg/util/polygon.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 class Instructions(Enum):
     No_Instruction = 0
     Polygon_Instruction = 1
-    # Moving = 2
 
 
 def get_polygon(label, sample=2):
@@ -20,11 +19,10 @@ def get_polygon(label, sample=2):
     # plt.imshow(label)
     # plt.savefig("./temp.png")
     # print("contours", contours[1])
-    # opencv3返回为3个参数，其中第二个是边界；opencv4和2只返回2个参数，其中第一个是边界
-    cv2_v = cv2.__version__.split('.')[0]
-    boundary = contours[1][0] if cv2_v == 3 else contours[0][0]
+    # 这个地方split出来是字符的3，不是数字，改了一下
+    cv2_v = cv2.__version__.split(".")[0]
+    boundary = contours[1][0] if cv2_v == "3" else contours[0][0]
     for p in boundary:
-        # print("+_+_+", p)
         if count == sample:
             points.append(p[0])
             count = 0
