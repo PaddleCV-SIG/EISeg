a1f679b1efeb4852377679d4400ec7ad5e9a729b
geoyee <geoyee@yeah.net>
2021-09-09 20:09:58 +0800

    add new rules to simplified boundary

--- eiseg/util/polygon.py
+++ eiseg/util/polygon.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 import cv2
-from math import sqrt
+import numpy as np
+import math
 
 
 class Instructions(Enum):
@@ -20,11 +21,14 @@ def get_polygon(label, sample="Dynamic"):
         polygons = []
         relas = []
         for contour, hierarchy in zip(contours, hierarchys[0]):
+            # opencv实现边界简化
             epsilon = 0.0005 * cv2.arcLength(contour, True) if sample == "Dynamic" else sample
             if not isinstance(epsilon, float) and not isinstance(epsilon, int):
                 epsilon = 0
-            print("epsilon:", epsilon)
+            # print("epsilon:", epsilon)
             out = cv2.approxPolyDP(contour, epsilon, True)
+            # 自定义边界简化
+            out = approx_poly_DP(out)
             # 判断自己，如果是子对象就不管自己是谁
             if hierarchy[2] == -1:
                 own = None
@@ -72,10 +76,61 @@ def _find_min_point(i_list, o_list):
     idx_o = -1
     for i in range(len(i_list)):
         for o in range(len(o_list)):
-            dis = sqrt((i_list[i][0] - o_list[o][0]) ** 2 + \
+            dis = math.sqrt((i_list[i][0] - o_list[o][0]) ** 2 + \
                        (i_list[i][1] - o_list[o][1]) ** 2)
             if dis < min_dis:
                 min_dis = dis
                 idx_i = i
                 idx_o = o
     return idx_i, idx_o
+
+
+# 根据三点坐标计算夹角
+def _cal_ang(p1, p2, p3):
+    eps = 1e-12
+    a = math.sqrt((p2[0] - p3[0]) * (p2[0] - p3[0]) + (p2[1] - p3[1]) * (p2[1] - p3[1]))
+    b = math.sqrt((p1[0] - p3[0]) * (p1[0] - p3[0]) + (p1[1] - p3[1]) * (p1[1] - p3[1]))
+    c = math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]))
+    ang = math.degrees(math.acos((b**2 - a**2 - c**2) / (-2 * a * c + eps)))  # p2对应
+    return ang
+
+
+# 边界点简化
+def approx_poly_DP(contour, min_dist=10, ang_err=10):
+    # print(contour.shape)  # N, 1, 2
+    cs = [contour[i][0] for i in range(contour.shape[0])]
+    ## 1. 先删除夹角接近180度的点
+    i = 0
+    while i < len(cs):
+        try:
+            last = (i - 1) if (i != 0) else (len(cs) - 1)
+            next = (i + 1) if (i != len(cs) - 1) else 0
+            ang_i = _cal_ang(cs[last], cs[i], cs[next])
+            if abs(ang_i) > (180 - ang_err):
+                del cs[i]
+            else:
+                i += 1
+        except:
+            i += 1
+    ## 2. 再删除两个相近点与前后两个点角度接近的点
+    i = 0
+    while i < len(cs):
+        try:
+            j = (i + 1) if (i != len(cs) - 1) else 0
+            if math.sqrt((cs[i][0] - cs[j][0]) ** 2 + \
+                        (cs[i][1] - cs[j][1]) ** 2) < min_dist:
+                last = (i - 1) if (i != 0) else (len(cs) - 1)
+                next = (j + 1) if (j != len(cs) - 1) else 0
+                ang_i = _cal_ang(cs[last], cs[i], cs[next])
+                ang_j = _cal_ang(cs[last], cs[j], cs[next])
+                # print(ang_i, ang_j)  # 角度值为-180到+180
+                if abs(ang_i - ang_j) < ang_err:
+                    del cs[j]
+                else:
+                    i += 1
+            else:
+                i += 1
+        except:
+            i += 1
+    res = np.array(cs).reshape([-1, 1, 2])
+    return res
