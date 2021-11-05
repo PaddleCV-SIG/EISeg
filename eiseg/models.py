2c6a939a8dfedc2e4a2f6b83c724c70003b12ba2
geoyee <geoyee@yeah.net>
2021-11-05 15:21:18 +0800

    Update

--- eiseg/models.py
+++ eiseg/models.py
@@ -22,7 +22,8 @@ class EISegModel:
             ValueError(" 模型和参数不匹配，请检查模型和参数是否加载错误")
         if not use_gpu:
             config.enable_mkldnn()
-            config.enable_mkldnn_bfloat16()
+            if paddle.fluid.core.supports_bfloat16():
+                config.enable_mkldnn_bfloat16()
             config.switch_ir_optim(True)
             config.set_cpu_math_library_num_threads(10)
         else:
