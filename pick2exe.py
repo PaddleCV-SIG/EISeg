09effd9819d8b7855f1a5e6e228339f9bef23880
Lin Han <me@linhan.ml>
2021-11-11 07:03:53 -0500

    clean up for merge

--- pick2exe.py
+++
@@ -1,9 +0,0 @@
-from qpt.executor import CreateExecutableModule
-
-if __name__ == "__main__":
-    module = CreateExecutableModule(
-        work_dir="github/EISeg", 
-        launcher_py_path="github/EISeg/eiseg/exe.py", 
-        save_path="github/out"
-    )
-    module.make()
--- tool/pypi.sh
+++
@@ -1,4 +0,0 @@
-rm dist/*
-python setup.py sdist bdist_wheel
-twine upload --repository testpypi dist/* --verbose
-# twine upload --repository-url https://upload.pypi.org/legacy/  dist/* --verbose
